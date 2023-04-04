da429a9caf384e4ef070caeac217fb8899f79d64
Roberto Meloni <94394668+RobertoRueMeloni@users.noreply.github.com>
2023-04-04 11:38:42 -0700

    last implementation

--- main.js
+++ main.js
@@ -1,42 +1,3 @@
-// const { app, BrowserWindow } = require('electron')
-
-// let win;
-
-// function createWindow (){
-//   win = new BrowserWindow({
-//     width:1200,
-//     height:1200,
-//     backgroundColor:'#ffffff'
-    
-//   })
-//   win.loadURL(`file://${__dirname}/dist/weather-app/index.html`)
-
-//   // DevTools
-//   win.webContents.openDevTools()
-
-//   win.on('closed', function(){
-//     win = null
-//   })
-// }
-
-// //Create window on electron inizialization
-// app.on('ready', createWindow)
-
-// //Quit when all window are closed
-// app.on('window-all-closed',function(){
-//   //on macOs specific close process
-//   if(process.platform !== 'darwin'){
-//     app.quit()
-//   } 
-// })
-
-// app.on('activate', function(){
-//   //macOs specific close process
-//   if(win === null){
-//     createWindow()
-//   }
-// })
-
 const { app, BrowserWindow, protocol } = require('electron');
 const path = require('path');
 
@@ -64,6 +25,8 @@ function createWindow (){
 
   // DevTools
   win.webContents.openDevTools()
+
+  //Minimize maximise and close functions
 
   win.on('closed', function(){
     win = null
